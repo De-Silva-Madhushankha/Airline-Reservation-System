af2e985740f87bb1bfa4f23dd07e419fa9e32524
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-09-27 21:17:48 +0530

    Updated Forgot Password Page

--- b-airlines-frontend/src/App.jsx
+++ b-airlines-frontend/src/App.jsx
@@ -8,10 +8,11 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Book from './pages/Book';
 import Help from './pages/Help';
-import ForgotPasswordPage from './components/ForgotPassword';
+import ForgotPassword from './pages/ForgotPassword';
 import Profile from './pages/Profile';
 import axiosSetup from './axiosSetup';
 import Schedule from './pages/Schedule';
+import NotFound from './pages/NotFound';
 
 
 
@@ -23,11 +24,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/not-found" element={<NotFound/>} />
           <Route path="/sign-up" element={<SignUp/>} />
           <Route path="/sign-in" element={<SignIn/>} />
           <Route path="/book" element={<Book/>} />
           <Route path="/help" element={<Help/>} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage/>} />
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
           <Route path="/schedule" element={<Schedule/>} />
           <Route path="/user/profile" element = {<Profile/>}/>
           <Route path="*" element={<Home/>} />
