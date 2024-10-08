a7d1039d8068b99bcd5d9093acedd7cec46d7af8
Shalikatkt <162815099+Shalikatkt@users.noreply.github.com>
2024-10-08 12:35:09 +0530

    Add cards

--- b-airlines-admin/src/components/InitialData.jsx
+++ b-airlines-admin/src/components/InitialData.jsx
@@ -14,7 +14,7 @@ export default function InitialData() {
       useEffect(() => {
         const fetchCounts = async () => {
           try {
-            const response = await axios.get('http://localhost:3001/api/users/load-initial-data');
+            const response = await axios.get('http://localhost:3001/api/admin/load-initial-data');
             setCounts(response.data);
           } catch (err) {
             setError('Error fetching counts');
@@ -31,21 +31,25 @@ export default function InitialData() {
         <div className="grid grid-cols-2 gap-4">
             <div className='m-6 z-40 '>
                 <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <p className="mb-2 text-align-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Users</p>
                     <h1 className="mb-2 text-align-center text-9xl font-bold tracking-tight text-gray-900 dark:text-white"> {counts.users}+</h1>
                 </a>
             </div>
             <div className='m-6 z-40 '>
                 <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <p className="mb-2 text-align-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Aircrafts</p>
                     <h1 className="mb-2 text-align-center text-9xl font-bold tracking-tight text-gray-900 dark:text-white"> {counts.aircrafts}+</h1>
                 </a>
             </div>
             <div className='m-6 z-40 '>
                 <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <p className="mb-2 text-align-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Routes</p>
                     <h1 className="mb-2 text-align-center text-9xl font-bold tracking-tight text-gray-900 dark:text-white"> {counts.routes}+</h1>
                 </a>
             </div>
             <div className='m-6 z-40 '>
                 <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <p className="mb-2 text-align-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Airports</p>
                     <h1 className="mb-2 text-align-center text-9xl font-bold tracking-tight text-gray-900 dark:text-white"> {counts.airports}+</h1>
                 </a>
             </div>
