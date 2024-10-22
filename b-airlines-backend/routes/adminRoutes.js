8f27b52bc9e9774ff5f6c7c78cea4fe1881ed30b
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-10-23 03:39:25 +0530

    admin user

--- b-airlines-backend/routes/adminRoutes.js
+++ b-airlines-backend/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { loginAdmin , loadInitialData , getCountbyDestination , getCountbyTime} from '../controllers/adminController.js';
 import { getAllAircrafts } from '../controllers/aircraftController.js';
-
+import { getAllUsers } from '../controllers/userController.js';
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.get('/load-initial-data', loadInitialData);
 router.get('/user-count-destination', getCountbyDestination)
 router.get('/passenger-count-time', getCountbyTime)
 router.get('/aircrafts', getAllAircrafts,);
+router.get('/users', getAllUsers);
 
 export default router;
