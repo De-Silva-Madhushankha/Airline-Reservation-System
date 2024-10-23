0a08158159b4bf30b01a35795fa4d2cb09d54027
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-10-24 01:48:02 +0530

    users unhide

--- b-airlines-admin/src/components/SlideNav.jsx
+++ b-airlines-admin/src/components/SlideNav.jsx
@@ -52,12 +52,12 @@ export default function SildeNav({ setActiveSection }) {
                     </button>
                 </li>
                 
-                {/* <li>
+                <li>
                     <button onClick={() => setActiveSection("users")} href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <FaUsers />
                         <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
                     </button>
-                </li> */}
+                </li>
                 <li>
                     <button onClick={() => setActiveSection("addAircraft")} href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <MdFlight />
