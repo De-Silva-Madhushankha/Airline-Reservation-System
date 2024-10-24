9e5bcdb6399d1c68cce4b778e2f91986692c5700
De-Silva-Madhushankha <madhushankhades1@gmail.com>
2024-10-24 20:05:12 +0530

    updated axios and db connection

--- b-airlines-backend/database/db.js
+++ b-airlines-backend/database/db.js
@@ -1,23 +1,17 @@
-import mysql from 'mysql2' // Using mysql2 for promise support
-import dotenv from 'dotenv'
+import mysql from 'mysql2'; // Using mysql2 with promise support
+import dotenv from 'dotenv';
 
-dotenv.config()
-
+dotenv.config();
 
 // Database connection configuration
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT, 
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
-}).promise()
+}).promise();
 
-const db = pool
-//const [raw]= await db.query("SELECT* FROM flight")
-// Test the connection
+const db = pool;
 console.log("Database connected");
-//console.log(raw);
-
 export default db;
-
-
--- b-airlines-frontend/src/axiosConfig.js
+++ b-airlines-frontend/src/axiosConfig.js
@@ -2,7 +2,6 @@ import Axios from 'axios';
 
 const axios = Axios.create({
   baseURL: 'http://localhost:3001/api', // API server
-  timeout: 1000, // Optional: Set a timeout for requests
 });
 
 export default axios;
