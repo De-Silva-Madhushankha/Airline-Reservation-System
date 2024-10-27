0db0cf43cb1e6204a2a9909a2bf8f8fa6003f3fb
Shalikatkt <162815099+Shalikatkt@users.noreply.github.com>
2024-10-27 23:59:39 +0530

    set logout buuton in admin page

--- b-airlines-admin/src/components/SlideNav.jsx
+++ b-airlines-admin/src/components/SlideNav.jsx
@@ -3,12 +3,15 @@ import { AiFillAccountBook, AiFillFileZip } from "react-icons/ai";
 import { MdOutlineKeyboardArrowDown, MdFlight, MdLogout, MdOutlineAddRoad} from "react-icons/md";
 import { RxDashboard } from "react-icons/rx";
 import { FaUsers } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 
 
 
 
 export default function SildeNav({ setActiveSection }) {
+    const navigate = useNavigate();
+
 
   return (
     <div>
@@ -52,13 +55,13 @@ export default function SildeNav({ setActiveSection }) {
                                 Passenger Count and manage Delay
                             </button>
                         </li>
-    <li>
-        <button onClick={() => setActiveSection("report5")} 
-                className="flex items-center w-full text-left text-gray-900 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-            Revenues By Aircrafts
-        </button>
-    </li>
-</ul>
+                        <li>
+                            <button onClick={() => setActiveSection("report5")} 
+                                    className="flex items-center w-full text-left text-gray-900 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                Revenues By Aircrafts
+                            </button>
+                        </li>
+            </ul>
 
                 </li>
                 <li>
@@ -81,10 +84,16 @@ export default function SildeNav({ setActiveSection }) {
                     </button>
                 </li>
                 <li>
-                    <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <button
+                        onClick={() => {
+                        // Any logout logic here (e.g., clearing tokens)
+                        navigate('/');
+                        }}
+                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    >
                         <MdLogout />
                         <span className="flex-1 ms-3 whitespace-nowrap">Log out</span>
-                    </a>
+                    </button>
                 </li>
                
             </ul>
