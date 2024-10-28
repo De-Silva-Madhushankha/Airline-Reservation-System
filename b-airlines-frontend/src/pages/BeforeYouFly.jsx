c352ddee79d21d64b9850406f094f983ab785bc1
Vithursanaa <vsanagv@gamil.com>
2024-10-28 19:52:18 +0530

    changes related to manage

--- b-airlines-frontend/src/pages/BeforeYouFly.jsx
+++ b-airlines-frontend/src/pages/BeforeYouFly.jsx
@@ -8,31 +8,12 @@ const BeforeYouFly = () => {
     <>
     <Navbar/>
     <Innercompon/>
-    <a href="/manage" style={styles.button}>Move-Back</a>
     <Footer />
     </>
 
   );
 };
 
-const styles = {
-  button: {
-    maxWidth: '220px',
-    margin: 'auto',
-    fontSize: '1.5em',
-    color: '#000',
-    backgroundColor: '#fff',
-    padding: '15px 20px',
-    textAlign: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    cursor: 'pointer',
-    textDecoration: 'none',
-    borderRadius: '8px',
-    border: '1px solid #ddd',
-    marginBottom: '10px',
-    transition: 'background-color 0.3s, color 0.3s',
-  },  
-};
+
 
 export default BeforeYouFly;
