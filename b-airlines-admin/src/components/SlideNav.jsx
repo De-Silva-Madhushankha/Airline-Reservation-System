7c957632a82d9b3e36304aee8023e5beb601bc5b
Shalikatkt <162815099+Shalikatkt@users.noreply.github.com>
2024-10-28 19:20:59 +0530

    edit css

--- b-airlines-admin/src/components/SlideNav.jsx
+++ b-airlines-admin/src/components/SlideNav.jsx
@@ -1,108 +1,102 @@
 import React from 'react'
 import { AiFillAccountBook, AiFillFileZip } from "react-icons/ai";
-import { MdOutlineKeyboardArrowDown, MdFlight, MdLogout, MdOutlineAddRoad} from "react-icons/md";
+import { MdOutlineKeyboardArrowDown, MdFlight, MdLogout, MdOutlineAddRoad } from "react-icons/md";
 import { RxDashboard } from "react-icons/rx";
 import { FaUsers } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 
-
-
-
-
-export default function SildeNav({ setActiveSection }) {
+export default function SideNav({ setActiveSection }) {
     const navigate = useNavigate();
 
-
-  return (
-    <div>
-        <aside id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            <ul className="space-y-2 font-medium">
-                <li>
-                    <button onClick={() => setActiveSection("dashboard")} href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <RxDashboard />
-                        <span className="ms-3">Dashboard</span>
-                    </button>
-                </li>
-                <li className=' group'>
-                    <button type="button" className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                        <AiFillFileZip />
-                        <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Reports</span>
-                        <MdOutlineKeyboardArrowDown />
-                    </button>
-                    <ul id="dropdown-example" className="hidden group-hover:block space-y-1 text-sm">
+    return (
+        <div>
+            <aside id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+                <div className="h-full px-3 py-4 overflow-y-auto bg-gray-900 text-white">
+                    <ul className="space-y-2 font-medium">
                         <li>
-                            <button onClick={() => setActiveSection("report1")} 
-                                    className="flex items-center w-full text-left text-gray-900 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                Below/Over age 18
+                            <button onClick={() => setActiveSection("dashboard")} 
+                                    className="flex items-center p-2 text-gray-100 rounded-lg hover:bg-gray-700 group">
+                                <RxDashboard />
+                                <span className="ms-3">Dashboard</span>
+                            </button>
+                        </li>
+                        <li className="group">
+                            <button type="button" 
+                                    className="flex items-center w-full p-2 text-gray-100 transition duration-75 rounded-lg group hover:bg-gray-700" 
+                                    aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                                <AiFillFileZip />
+                                <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Reports</span>
+                                <MdOutlineKeyboardArrowDown />
+                            </button>
+                            <ul id="dropdown-example" className="hidden group-hover:block space-y-1 text-sm">
+                                <li>
+                                    <button onClick={() => setActiveSection("report1")} 
+                                            className="flex items-center w-full text-left text-gray-300 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-700">
+                                        Below/Over age 18
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => setActiveSection("report2")} 
+                                            className="flex items-center w-full text-left text-gray-300 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-700">
+                                        Num of passenger Travel to given destination
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => setActiveSection("report3")} 
+                                            className="flex items-center w-full text-left text-gray-300 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-700">
+                                        Num of Booking By passenger Types
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => setActiveSection("report4")} 
+                                            className="flex items-center w-full text-left text-gray-300 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-700">
+                                        Passenger Count and manage Delay
+                                    </button>
+                                </li>
+                                <li>
+                                    <button onClick={() => setActiveSection("report5")} 
+                                            className="flex items-center w-full text-left text-gray-300 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-700">
+                                        Revenues By Aircrafts
+                                    </button>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <button onClick={() => setActiveSection("addFlights")} 
+                                    className="flex items-center p-2 text-gray-100 rounded-lg hover:bg-gray-700 group">
+                                <MdOutlineAddRoad />
+                                <span className="flex-1 ms-3 whitespace-nowrap">Add Flights</span>
                             </button>
                         </li>
                         <li>
-                            <button onClick={() => setActiveSection("report2")} 
-                                    className="flex items-center w-full text-left text-gray-900 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                Num of passenger Travel to given destination
+                            <button onClick={() => setActiveSection("users")} 
+                                    className="flex items-center p-2 text-gray-100 rounded-lg hover:bg-gray-700 group">
+                                <FaUsers />
+                                <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
                             </button>
                         </li>
                         <li>
-                            <button onClick={() => setActiveSection("report3")} 
-                                    className="flex items-center w-full text-left text-gray-900 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                Num of Booking By passenger Types
+                            <button onClick={() => setActiveSection("addAircraft")} 
+                                    className="flex items-center p-2 text-gray-100 rounded-lg hover:bg-gray-700 group">
+                                <MdFlight />
+                                <span className="flex-1 ms-3 whitespace-nowrap">Add Aircrafts</span>
                             </button>
                         </li>
                         <li>
-                            <button onClick={() => setActiveSection("report4")} 
-                                    className="flex items-center w-full text-left text-gray-900 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                Passenger Count and manage Delay
+                            <button
+                                onClick={() => {
+                                    // Add any logout logic here (e.g., clearing tokens)
+                                    navigate('/');
+                                }}
+                                className="flex items-center p-2 text-gray-100 rounded-lg hover:bg-gray-700 group"
+                            >
+                                <MdLogout />
+                                <span className="flex-1 ms-3 whitespace-nowrap">Log out</span>
                             </button>
                         </li>
-                        <li>
-                            <button onClick={() => setActiveSection("report5")} 
-                                    className="flex items-center w-full text-left text-gray-900 transition duration-75 rounded-lg pl-4 py-2 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-                                Revenues By Aircrafts
-                            </button>
-                        </li>
-            </ul>
-
-                </li>
-                <li>
-                    <button onClick={() => setActiveSection("addFlights")}  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <MdOutlineAddRoad />
-                        <span className="flex-1 ms-3 whitespace-nowrap">Add Flights</span>
-                    </button>
-                </li>
-                
-                <li>
-                    <button onClick={() => setActiveSection("users")} href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <FaUsers />
-                        <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
-                    </button>
-                </li>
-                <li>
-                    <button onClick={() => setActiveSection("addAircraft")} href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <MdFlight />
-                        <span className="flex-1 ms-3 whitespace-nowrap">Add aircrafts</span>
-                    </button>
-                </li>
-                <li>
-                    <button
-                        onClick={() => {
-                        // Any logout logic here (e.g., clearing tokens)
-                        navigate('/');
-                        }}
-                        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                    >
-                        <MdLogout />
-                        <span className="flex-1 ms-3 whitespace-nowrap">Log out</span>
-                    </button>
-                </li>
-               
-            </ul>
+                    </ul>
+                </div>
+            </aside>
         </div>
-    </aside>
-    
-    </div>
-
-    
-
-  )
+    )
 }
