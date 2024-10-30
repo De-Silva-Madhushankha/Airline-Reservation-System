c7b0124364b6c39ca4efa91e47d9207a6635bd48
PamojX <157775896+PamojX@users.noreply.github.com>
2024-10-30 15:26:29 +0530

    passenger details fetched to cancel booking table from user_bookings view

--- b-airlines-backend/models/bookingModel.js
+++ b-airlines-backend/models/bookingModel.js
@@ -107,7 +107,7 @@ const Booking = {
 
     getBookingByUserId: async (id) => {
         console.log('hey',id)
-        const [rows] = await db.query(`SELECT * FROM Booking WHERE user_id = ?`, [id])
+        const [rows] = await db.query(`SELECT * FROM user_bookings WHERE user_id = ?`, [id])
         console.log(rows)
         return rows;
     },  
